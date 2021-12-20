5d5bc0f256eab4c260c994f127fa750006a4fc94
NaveengrXworkz <naveengr.xworkz@gmail.com>
2021-12-20 18:03:14 +0530

    char programme

---
+++ Kilometers.java
@@ -0,0 +1,15 @@
+class Kilometers{
+      public static void main(String []args){
+      long []kms = {11,18,23,86,19,56,72,99,101,};
+      System.out.println(kms[0]);
+      System.out.println(kms[1]);
+      System.out.println(kms[2]);
+      System.out.println(kms[3]);
+      System.out.println(kms[4]);
+      System.out.println(kms[5]);
+      System.out.println(kms[6]);
+      System.out.println(kms[7]);
+      System.out.println(kms[8]);
+      System.out.println(kms[9]);
+      }
+}
